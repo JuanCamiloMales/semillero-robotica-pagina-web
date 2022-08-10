41fb3ad0ed61d1c4ed4bdfc78354b21700bafe65
JuanCamiloMales <jucamac@unicauca.edu.co>
2022-08-10 15:03:03 -0500

    Logo

--- src/componentes/Header/Logo/Logo.js
+++ src/componentes/Header/Logo/Logo.js
@@ -3,7 +3,7 @@ import './Logo.css'
 
 export default function Logo(){
     return (
-        <a href='/' className='Logo'>
+        <a href='/semillero-robotica-pagina-web' className='Logo'>
             <img className='Logo-img' src='Logo-img.svg' alt='Logo'/>
         </a>
     )
