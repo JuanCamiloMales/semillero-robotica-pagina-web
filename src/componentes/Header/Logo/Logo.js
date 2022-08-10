0eca914ec0726e34e4ee0f9e9488785af4d6845f
JuanCamiloMales <jucamac@unicauca.edu.co>
2022-08-10 14:58:51 -0500

    Logo

--- src/componentes/Header/Logo/Logo.js
+++ src/componentes/Header/Logo/Logo.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link } from 'wouter'
 import './Logo.css'
 
 export default function Logo(){
     return (
-        <Link to={'/'} className={'Logo'}>
-                <img className='Logo-img' src='Logo-img.svg' alt='Logo'/>
-        </Link>
+        <a href='/' className='Logo'>
+            <img className='Logo-img' src='Logo-img.svg' alt='Logo'/>
+        </a>
     )
 }
